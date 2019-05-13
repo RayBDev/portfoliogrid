ea31ea690d7c50f46929166eda960f473f38bd00
RayB <raybernard1337@gmail.com>
2019-05-13 08:03:48 -0600

    First Commit

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteMetadata: {
+    title: `Portfolio Grid`,
+    description: `Portfolio Grid is a portfolio website template for freelancers written in Gatsby using Bootstrap.`,
+    author: `Ray Bernard`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/components/common/Navigation.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Navigation = () => {
+  return <section id="navigation" />
+}
+
+export default Navigation
