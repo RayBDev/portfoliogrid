81b33e89bc34bcb04935f6bd603fb52efccf6df2
RayB <raybernard1337@gmail.com>
2019-05-14 07:19:31 -0600

    Added resume section

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -14,6 +14,70 @@ const Resume = ({ isResumeOpen }) => {
               Adipisci, blanditiis.
             </p>
           </div>
+
+          <div className="card card-body py-5">
+            <h3>Where Have I Worked?</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+              facere odit debitis, vero et cupiditate!
+            </p>
+            <div className="card-deck">
+              <div className="card">
+                <div className="card-body">
+                  <h4 className="card-title">DevMasters</h4>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Doloribus, vel.
+                  </p>
+                  <p className="p-2 mb-3 bg-dark text-white">
+                    Position: Full Stack Developer
+                  </p>
+                  <p className="p-2 mb-3 bg-dark text-white">
+                    Phone: (555) 555-5555
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <h6 className="text-muted">Dates: 2015 - 2019</h6>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-body">
+                  <h4 className="card-title">123 Designs</h4>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Doloribus, vel.
+                  </p>
+                  <p className="p-2 mb-3 bg-dark text-white">
+                    Position: Web Designer
+                  </p>
+                  <p className="p-2 mb-3 bg-dark text-white">
+                    Phone: (444) 444-4444
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <h6 className="text-muted">Dates: 2013 - 2015</h6>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-body">
+                  <h4 className="card-title">Web Designer Pros</h4>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Doloribus, vel.
+                  </p>
+                  <p className="p-2 mb-3 bg-dark text-white">
+                    Position: Web Designer
+                  </p>
+                  <p className="p-2 mb-3 bg-dark text-white">
+                    Phone: (333) 333-3333
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <h6 className="text-muted">Dates: 2010 - 2013</h6>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
       </div>
     </Collapse>
